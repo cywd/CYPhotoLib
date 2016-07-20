67c92075fb9d74a27866b8aefdba89a3bd61ac0e
CoderCYLee <lichunyang@outlook.com>
2016-07-20 13:56:33 +0800

    1.修改导航栏背景色为蓝色 2.修改AlertView为AlertController 3.添加用户拒绝照片权限时，提醒界面可以跳转到设置-隐私-照片

--- PhotoLibDemo/PhotoLibDemo/CYPhotoLib/Classes/CYPhotoPicker/Controller/CYPhotoBrowserController.m
+++ PhotoLibDemo/PhotoLibDemo/CYPhotoLib/Classes/CYPhotoPicker/Controller/CYPhotoBrowserController.m
@@ -88,6 +88,8 @@
     }];
     
     
+    
+    
     return cell;
 }
 
--- PhotoLibDemo/PhotoLibDemo/ViewController.m
+++ PhotoLibDemo/PhotoLibDemo/ViewController.m
@@ -34,8 +34,22 @@
 - (IBAction)btnClick:(UIButton *)sender {
     
     if ([PHPhotoLibrary authorizationStatus] != PHAuthorizationStatusAuthorized) {
-        UIAlertView * photoLibaryNotice = [[UIAlertView alloc]initWithTitle:@"应用程序无访问照片权限" message:@"请在“设置\"-\"隐私\"-\"照片”中设置允许访问" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:@"设置", nil];
-        [photoLibaryNotice show];
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"应用程序无访问照片权限" message:@"请在“设置\"-\"隐私\"-\"照片”中设置允许访问" preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"设置" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=Privacy&path=PHOTOS"]];
+        }];
+        [alert addAction:cancelAction];
+        [alert addAction:okAction];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+        
+        
+//        UIAlertView * photoLibaryNotice = [[UIAlertView alloc]initWithTitle:@"应用程序无访问照片权限" message:@"请在“设置\"-\"隐私\"-\"照片”中设置允许访问" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:@"设置", nil];
+//        [photoLibaryNotice show];
+        
         return;
     }
     CYPhotoAblumListController * ablumsList = [[CYPhotoAblumListController alloc]init];
